5137e30913b060a8c57e5a352c07705b7d5918ac
Scott Ladenheim <saladenh@bu.edu>
2023-12-20 12:25:23 -0500

    Refactoring of code, adding tests, updated .csv files

--- assignmentPandas.py
+++ assignmentPandas.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 
 
@@ -7,5 +6,5 @@ Implement a function that reads in two datatable filenames,  inner joins the two
 tables on the customerID field, and returns the joined tables as a dataframe.
 """
 def problem1(filename1, filename2):
-
+    
     return 
